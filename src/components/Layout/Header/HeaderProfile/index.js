e5aa352691b8a9fe8822d29a451f95b04db101b7
oyhun00 <oyhun00@gmail.com>
2021-01-22 18:08:09 +0900

    feat: redux 구조 변경 진행

--- src/components/Layout/Header/HeaderProfile/index.js
+++ src/components/Layout/Header/HeaderProfile/index.js
@@ -1,70 +1,24 @@
-import React, { useEffect, useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as HeaderActions from '../../../../reducers/Layout/Header/HeaderDropDown';
+import HeaderProfile from './HeaderProfile';
 
-import * as actions from '../../../../actions/HeaderDropDown';
-
-import styled from 'styled-components';
-import { Dropdown, DropdownMenu, DropdownToggle } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import HeaderProfileBox from './HeaderProfileBox';
 
 const HeaderProfile = () => {
-  const { isOpen } = useSelector((state) => state.HeaderDropDown);
-
-  const dispatch = useDispatch();
-  const dropDown = useCallback(() => {
-    dispatch(actions.HeaderDropDown());
-  }, [dispatch]);
-
-  useEffect(() => {
-  }, []);
-
+  const mapStateToProps = (state) => ({
+    isOpen: state.isOpen,
+  })
+  
+  const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(HeaderActions, dispatch);
+  }
+  
+  
   return (
-    <Dropdown isOpen={isOpen} toggle={dropDown}>
-      <DropdownToggle tag="div" data-toggle="dropdown" aria-expanded={isOpen}>
-        <ProfileIcons icon={faUser} />
-      </DropdownToggle>
-      <DropdownMenuCustom right>
-        <HeaderProfileBox />
-      </DropdownMenuCustom>
-    </Dropdown>
+    <HeaderProfile props={mapStateToProps, mapDispatchToProps} />
   );
 }
 
-const ProfileIcons = styled(FontAwesomeIcon)`
-  color: #bdbdbd;
-  cursor: pointer;
-  user-select: none;
-  width: 0.675em !important;
-
-  :hover {
-    opacity: 0.7;
-  }
-
-  @media (min-width: 992px) {
-    width: 0.8em !important;
-  }
-`;
-
-const DropdownMenuCustom = styled(DropdownMenu)`
-  padding: 0;
-  font-size: 0.4rem;
-  width: 14rem;
-  border: 0;
-  box-shadow: 0 5px 13px 0px rgba(252, 214, 97, 0.54);
-
-  @media (min-width: 768px) {
-    font-size: 0.8rem;
-    width: 20rem;
-  }
-
-  @media (min-width: 992px) {
-    width: 24rem;
-  }
-
-  @media (min-width: 1200px) {
-  }
-`;
 
 export default HeaderProfile;
--- src/reducers/Layout/Header/HeaderDropDown.js
+++ src/reducers/Layout/Header/HeaderDropDown.js
@@ -1,19 +1,33 @@
+import { createAction, handleActions } from 'redux-actions';
 import * as HeaderDropDown from '../../../actions/HeaderDropDown';
+
+const DROPDOWN_OPEN = 'header/DROPDOWN_OPEN';
+
+export const headerDropDown = createAction(DROPDOWN_OPEN);
 
 const initialState = {
   isOpen: false,
 };
 
-const reducers = (state = initialState, action) => {
-  const { type } = action;
-  switch (type) {
-    case HeaderDropDown.DROPDOWN_OPEN: {
-      return { isOpen: !state.isOpen };
-    }
-    default: {
-      return state;
+// const reducers = (state = initialState, action) => {
+//   const { type } = action;
+//   switch (type) {
+//     case HeaderDropDown.DROPDOWN_OPEN: {
+//       return { isOpen: !state.isOpen };
+//     }
+//     default: {
+//       return state;
+//     }
+//   }
+// }
+
+export default handleActions({
+  [DROPDOWN_OPEN]: ({ isOpen }) => {
+    return {
+      ...state,
+      isOpen: !isOpen
     }
   }
-}
+}, initialState)
 
 export default reducers;
